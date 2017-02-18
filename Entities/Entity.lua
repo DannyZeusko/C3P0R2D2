948e75e109245cc1cab65dc6561e18d8913ef2a4
Zac-onebillion <zackorzinski@hotmail.com>
2017-02-18 16:57:24 -0500

    I'm not super sure what's changed

--- Entities/Entity.lua
+++ Entities/Entity.lua
@@ -1,3 +1,5 @@
+local baton = require ("Assets/baton")
+
 self = {} -- after some consideration, and in the absence fo any way to do inheritence or polymorphism quickly, 
           -- We should make one object type that has litterally all logic and state that any object in the game can have
 
@@ -7,10 +9,13 @@ self.y = 0.0
 self.speedx = 0.0
 self.speedy = 0.0
 
-self.sprite = nil
+self.spriteUp = nil
+self.spriteDown = nil
+self.facing = 1.0
 
 self.has_ball = false
 self.is_ball = false
+self.bal_thrown = false
 
 function enemyMove()
     
@@ -19,5 +24,15 @@ end
 function playerMove()
 
 end
+
+function ballMove()
+
+end
+
+function throwBall()
+
+end
+
+
 
 return self
