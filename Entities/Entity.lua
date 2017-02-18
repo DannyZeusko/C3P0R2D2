db5ef7e4e2d1c6fdad7b2d06b711206bdb2a104b
Zac-onebillion <zackorzinski@hotmail.com>
2017-02-18 17:08:31 -0500

    input handling

--- Entities/Entity.lua
+++ Entities/Entity.lua
@@ -1,4 +1,5 @@
 local baton = require ("Assets/baton")
+local input
 
 self = {} -- after some consideration, and in the absence fo any way to do inheritence or polymorphism quickly, 
           -- We should make one object type that has litterally all logic and state that any object in the game can have
@@ -17,6 +18,27 @@ self.has_ball = false
 self.is_ball = false
 self.bal_thrown = false
 
+if playerno == 1 then
+    local controls = {
+        left = {'key:d', 'axis:leftx-', 'button:dpleft'},
+        right = {'key:a', 'axis:leftx+', 'button:dpright'},
+        up = {'key:w', 'axis:lefty-', 'button:dpup'},
+        down = {'key:d', 'axis:lefty+', 'button:dpdown'},
+        dash = {'key:x', 'button:a'},
+        attack = {'key:z', 'button:b'}
+    }
+    local input = baton.new(controls, love.joystick.getJoysticks()[1])
+else
+    local controls = {
+        left = {'key:left', 'axis:leftx-', 'button:dpleft'},
+        right = {'key:right', 'axis:leftx+', 'button:dpright'},
+        up = {'key:up', 'axis:lefty-', 'button:dpup'},
+        down = {'key:down', 'axis:lefty+', 'button:dpdown'},
+        dash = {'key:,', 'button:a'},
+        attack = {'key:.', 'button:b'}
+    }
+    local input = baton.new(controls, love.joystick.getJoysticks()[2])
+end
 function enemyMove()
     
 end
